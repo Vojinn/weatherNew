41949fc1f9cab4d0f4352d88d2cc587d7bc11172
Vojin <dvojinovic38@gmail.com>
2023-03-14 00:05:28 +0100

    initial commit

---
+++ WeatherNew/WeatherNew/WeatherNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNewApp.swift
+//  WeatherNew
+//
+//  Created by Dusan Vojinovic on 14.3.23..
+//
+
+import SwiftUI
+
+@main
+struct WeatherNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
